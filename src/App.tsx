0e4f28f6155199da94e94e7d2055c72a544f684d
rsaban <sabanr@gmail.com>
2025-04-24 01:35:55 -0300

    feat: implement Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
+import Search from './components/Search';
+
 const App: React.FC = () => {
 	return (
-		<>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-				expedita, placeat tempora veniam autem deserunt reiciendis porro
-				rem delectus possimus asperiores commodi aperiam eligendi optio
-				dolores unde incidunt amet! Error.
-			</div>
-		</>
+		<div className="App">
+			<Search />
+		</div>
 	);
 };
-
 export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,8 @@
+const Search: React.FC = () => {
+	return (
+		<div>
+			<input type="text" title="Search" />
+		</div>
+	);
+};
+export default Search;
