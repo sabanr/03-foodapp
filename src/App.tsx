386b08055ae330d3364600f5ec9602dab130dca7
rsaban <sabanr@gmail.com>
2025-04-24 00:52:43 -0300

    refactor: update App component structure and remove unused assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-	return <></>;
-}
+const App: React.FC = () => {
+	return (
+		<>
+			<div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+				expedita, placeat tempora veniam autem deserunt reiciendis porro
+				rem delectus possimus asperiores commodi aperiam eligendi optio
+				dolores unde incidunt amet! Error.
+			</div>
+		</>
+	);
+};
 
 export default App;
